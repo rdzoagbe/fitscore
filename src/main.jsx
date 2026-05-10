3be9d351b4609326504a6dcbc5dfe28203d767f4
Roland Dzoagbe <32632228+rdzoagbe@users.noreply.github.com>
2026-05-10 08:05:28 +0200

    stability: wrap app in error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
 import { LangProvider } from './context/LangContext'
+import ErrorBoundary from './components/ErrorBoundary'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <LangProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </LangProvider>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <LangProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </LangProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 )
